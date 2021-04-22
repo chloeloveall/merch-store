9cac639a6af57e59dfaf0280989a789ed8361e9b
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-21 19:13:35 -0700

    Add static subFooter with compuright info

---
+++ src/components/Footer/SubFooter.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: '#000000',
+    color: '#ffffff',
+    maxHeight: '2em',
+  },
+  // menuButton: {
+  //   marginRight: theme.spacing(2),
+  // },
+  title: {
+    flexGrow: 1,
+    margin: 'auto',
+    padding: '.25em'
+  },
+  linkStyle: {
+    color: 'inherit'
+  }
+}));
+
+function SubFooter() {
+
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="static" className={classes.root}>
+          <p className={classes.title}>COPYRIGHT &copy; <a className={classes.linkStyle} href = "mailto: chloeloveall@protonmail.com">CHLOE LOVEALL</a> 2021</p>
+      </AppBar>
+    </>
+  );
+}
+
+export default SubFooter;

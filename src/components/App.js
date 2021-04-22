ac8d4ed67cc75f8f48b6a42f9bb60ddbcd987b5b
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-21 19:09:57 -0700

    Import header and footer into App.js

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
+

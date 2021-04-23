2fccfd41c6f8651135ba321df5b0b5dde6b2e939
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-22 20:12:41 -0700

    Add main content compent with ImageCarousel imported

---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageCarousel from './ImageCarousel';
+
+function Main() {
+  
+  return (
+    <>
+      <ImageCarousel />
+    </>
+  );
+}
+
+export default Main;

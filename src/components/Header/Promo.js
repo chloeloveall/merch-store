c05a35ff33259b619f5f02dc003f416b2bc27d15
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-22 20:21:45 -0700

    Add promo bar component to header

---
+++ src/components/Header/Promo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: '#EAFF00',
+    color: '#000000',
+    maxHeight: '2em',
+    position: 'fixed'
+  },
+  title: {
+    flexGrow: 1,
+    margin: 'auto',
+    padding: '.25em',
+    fontWeight: 'bold'
+  },
+}));
+
+function Promo() {
+
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="static" className={classes.root}>
+          <p className={classes.title}>FREE SHIPPING ON ALL ORDERS!</p>
+      </AppBar>
+    </>
+  )
+}
+
+export default Promo

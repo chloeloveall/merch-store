488047a86491bd0097c16c1fdf0899cde9a0af75
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-22 20:19:58 -0700

    Add search bar component for header

---
+++ src/components/Header/Search.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Input } from 'semantic-ui-react'
+
+function Search() {
+  return (
+    <>
+      <Input
+        // action={{ color: 'blue', content: 'Search' }}
+        icon="search"
+        iconPosition="right"
+        placeholder="Search"
+      />
+    </>
+  )
+}
+
+export default Search

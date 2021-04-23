d0f3add5e83ad727a304f58c0ce0039e0e665890
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-22 21:04:52 -0700

    Refactor

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ImageCarousel from './ImageCarousel';
+import React from 'react'
+import ImageCarousel from './ImageCarousel'
 
 export default function Main() {
-  
   return (
     <>
       <ImageCarousel />
     </>
-  );
+  )
 }
